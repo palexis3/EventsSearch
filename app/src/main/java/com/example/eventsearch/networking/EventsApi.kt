58b0418d02c9ce49b8d4ebb7d33446758543f12b
Patrick Alexis <alexispatrick77@gmail.com>
2022-12-04 18:32:33 -0800

    Added api key in local.properties file

--- app/src/main/java/com/example/eventsearch/networking/EventsApi.kt
+++ app/src/main/java/com/example/eventsearch/networking/EventsApi.kt
@@ -1,12 +1,13 @@
 package com.example.eventsearch.networking
 
+import com.example.eventsearch.BuildConfig
 import com.example.eventsearch.model.SearchResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface EventsApi {
 
-    @GET("/discovery/v2/events.json?city=San Francisco&apikey=")
+    @GET("/discovery/v2/events.json?city=San Francisco&apikey=${BuildConfig.API_KEY}")
     suspend fun search(
         @Query("keyword") keyword: String
     ) : SearchResponse
