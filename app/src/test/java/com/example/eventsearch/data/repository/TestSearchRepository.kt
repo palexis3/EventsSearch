e52d3490fc95d9cdb18bce01cb367a24ffc43673
Patrick Alexis <alexispatrick77@gmail.com>
2022-12-07 10:17:00 -0800

    Added testing repository

---
+++ app/src/test/java/com/example/eventsearch/data/repository/TestSearchRepository.kt
@@ -0,0 +1,28 @@
+package com.example.eventsearch.data.repository
+
+import com.example.eventsearch.data.model.EventUi
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+
+class TestSearchRepository : SearchRepository {
+    /**
+     * A hot testing flow to emit list of EventUi models
+     */
+    private val eventUiFlow: MutableSharedFlow<List<EventUi>> =
+        MutableSharedFlow(replay = 1, onBufferOverflow = BufferOverflow.DROP_OLDEST)
+
+    override fun search(keyword: String): Flow<List<EventUi>> = eventUiFlow
+
+    /**
+     * No operation for testing since this is a network call
+     */
+    override suspend fun refresh(keyword: String) {}
+
+    /**
+     * A testing function to allow the control of list of EventUi models
+     */
+    suspend fun sendEventUis(eventUis: List<EventUi>) {
+        eventUiFlow.emit(eventUis)
+    }
+}
