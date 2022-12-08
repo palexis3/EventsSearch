c0e84f37979fc5204d1dd17a13eefef70fd2976a
Patrick Alexis <alexispatrick77@gmail.com>
2022-12-08 11:29:30 -0800

    Add build annotation to onCreate lifecycle method

--- app/src/main/java/com/example/eventsearch/MainActivity.kt
+++ app/src/main/java/com/example/eventsearch/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.eventsearch
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import com.example.eventsearch.ui.composable.SearchScreen
 import com.example.eventsearch.ui.theme.EventSearchTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
